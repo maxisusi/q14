32cfe19ebbcccbea8389af1170f9cd34864aaabb
max <maxbastienbalej@gmail.com>
2023-09-24 13:54:42 +0200

    Adding click framework to manage CLI options

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from parser import format_menu, fetch_page
 from ui import display_menu 
+import click
 
-def main():
-    debug_mode = True
+@click.command()
+@click.option('--debug','-d', is_flag=True, help='Display all the menus')
+def main(debug):
+    debug_mode = debug 
     soup = fetch_page(debug_mode)
     dict_plates = format_menu(soup)
     display_menu(dict_plates)
--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,30 +1,30 @@
 from termcolor import colored
-
+import click
 from lib import cleanup_terminal
 
 def display_menu(dict_plates: dict[str, list[str]]):
     cleanup_terminal()
     for key, value in dict_plates.items():
-        print(f"☀️  {key}")
-        print("")
+        click.echo(f"☀️  {key}")
+        click.echo("")
         if value[0] == None:
             close = colored("Restaurant fermé", 'grey')
-            print(close)
+            click.echo(close)
         else: 
             meat = colored(value[0], 'blue')
             veggie = colored(value[1], 'green')
-            print(f"🥩: {meat}")
-            print(f"🥬: {veggie}")
-        print("-------------------------")
+            click.echo(f"🥩: {meat}")
+            click.echo(f"🥬: {veggie}")
+        click.echo("-------------------------")
     exit()
 
 def display_close():
     cleanup_terminal()
     warning_text= colored("😎 Profitez du weekend, les plats arrivent!", 'red')
-    print(warning_text)
+    click.echo(warning_text)
     exit()
 
 def display_loading():
     cleanup_terminal()
-    print("🍽️ Menu de la semaine...")
+    click.echo("🍽️ Menu de la semaine...")
 
